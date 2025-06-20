7677b85f3aa6a1fdeb3503efc02f4fc3dbd325d5
Kyryl <kyryll@wix.com>
2025-06-20 17:02:11 +0300

    Added a line that shows the current time, and the actual time from 12am to 11pm

---
+++ static/js/index.js
@@ -0,0 +1,36 @@
+let redLine = null;
+let currentTime = 0;
+
+window.onload = (e) => {
+    redLine = getRedLine();
+};
+
+function getRedLine() {
+    return document.getElementById("red-line");
+}
+
+
+function setRedLinePos(number) {
+    if (redLine == null) {
+        redLine = getRedLine();
+
+        return;
+    }
+
+    redLine.style.top = number + "%";
+}
+
+function changeLinePos() {
+    currentTime += 0.005;
+    if (currentTime >= Math.PI) {
+        currentTime = 0;
+    }
+
+    let y = Math.pow(Math.sin(currentTime), 2) * 98;
+
+    setRedLinePos(y);
+}
+
+
+setInterval(changeLinePos, 0.1);
+
