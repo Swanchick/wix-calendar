4a2be7708feb8cac05ceb4ee689b7a3dd97c3c6b
Kyryl <kyryll@wix.com>
2025-06-23 16:09:42 +0300

    Added some stuff to better generate the calendar week

--- static/js/globals.js
+++ static/js/globals.js
@@ -1,5 +1,34 @@
 const WEEK_DATES_ID = "week-dates";
 const DAYS_CONTAINER_ID = "days-container";
+const ARROW_ID = "arrow";
+const WEEK_NAMES_CONTAINER = "week-names-container";
 
-const ARROW_ID = "red-line";
+const DAY_NAMES = [
+    "SUN",
+    "MON",
+    "TUE",
+    "WED",
+    "THU",
+    "FRI",
+    "SAT"
+];
 
+const MONTHS = [
+    31, // January
+    0, // February (28 or 29 depends on a year, but we will check it later)
+    31, // March
+    30, // April
+    31, // May
+    30, // June
+    31, // July
+    31, // August
+    30, // September
+    31, // October
+    30, // November
+    31, // December
+]
+
+const ALL_MONTHS = 12;
+
+const DAYS_IN_WEEK = 7;
+const HOURS_IN_DAY = 25;
--- static/js/week.js
+++ static/js/week.js
@@ -1,11 +1,11 @@
-
-
 class Week {
+    #weekNamesContainer;
     #weekDatesContainer;
     #dayContainer;
     #dayElements;
 
     constructor() {
+        this.#weekNamesContainer = document.getElementById(WEEK_NAMES_CONTAINER);
         this.#weekDatesContainer = document.getElementById(WEEK_DATES_ID);
         this.#dayContainer = document.getElementById(DAYS_CONTAINER_ID);
         this.#dayElements = [];
@@ -15,17 +15,84 @@ class Week {
         const currentDayInWeek = today.getDay();
         const currentDayInMonth = today.getDate();
 
+        this.#buildWeekNames(currentDayInWeek);
+        this.#buildWeekDays(currentDayInMonth, currentDayInWeek);
+        this.#buildDays(currentDayInWeek);
+        
+    }
+
+    #buildWeekNames(currentDayInWeek) {
+        if (this.#weekNamesContainer == null) {
+            return;
+        }
+
+        for (let i = 0; i < DAYS_IN_WEEK; i++) {
+            const weekNameElement = this.#createWeekNameElement(i, currentDayInWeek);
+            this.#weekNamesContainer.appendChild(weekNameElement);
+        }
+    }
+
+    #buildWeekDays(currentDayInMonth, currentDayInWeek) {
+        if (this.#weekDatesContainer == null) {
+            return;
+        }
+        
+        const firstDay = currentDayInMonth - currentDayInWeek;
+
+        for (let i = 0; i < DAYS_IN_WEEK; i++) {
+            const dayInMonth = firstDay + i;
+
+            const dayElement = this.#createWeekDayElement(dayInMonth, i, currentDayInWeek);
+            this.#weekDatesContainer.appendChild(dayElement);
+        }
+    }
+
+    #buildDays(currentDayInWeek) {
         if (this.#dayContainer == null) {
             return;
         }
 
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < DAYS_IN_WEEK; i++) {
             const isCurrentDay = i == currentDayInWeek;
-            const day = this.#createDayElement(isCurrentDay);
+            const isLastDay = i == DAYS_IN_WEEK - 1;
+            const day = this.#createDayElement(isCurrentDay, isLastDay);
 
             this.#dayContainer.appendChild(day);
             this.#dayElements.push(day);
         }
+    }
+
+    #createElementForWeek(day, currentDay) {
+        const element = document.createElement("h2");
+        
+        if (day != DAYS_IN_WEEK - 1) {
+            element.classList.add("right-border");
+        }
+
+        if (day == currentDay) {
+            element.classList.add("current-day");
+        }
+
+        return element;
+    }
+
+    #createWeekDayElement(day, weekIndex, currentDayInWeek) {
+        const element = this.#createElementForWeek(weekIndex, currentDayInWeek);
+        const textNode = document.createTextNode(day.toString());
+
+        element.appendChild(textNode);
+
+        return element;
+    }
+
+    #createWeekNameElement(day, currentDay) {
+        const element = this.#createElementForWeek(day, currentDay);
+        
+        const currentDayName = DAY_NAMES[day];
+        const textNode = document.createTextNode(currentDayName);
+        element.appendChild(textNode);
+
+        return element;
     }
 
     #createArrowElement() {
@@ -35,10 +102,12 @@ class Week {
         return arrowElement;
     }
 
-    #createDayElement(currentDay) {
+    #createDayElement(currentDay, lastDay) {
         const dayElement = document.createElement("div");
         dayElement.classList.add("day-container");
-        dayElement.classList.add("right-border");
+        if (!lastDay) {
+            dayElement.classList.add("right-border");
+        }
 
         const timeElements = this.#createTimeElements();
         
@@ -82,5 +151,34 @@ class Week {
         }
 
         return timeElements;
+    }
+
+    #isLeapYear(year) {
+        return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
+    }
+
+    #getFebruaryDays(year) {
+        return this.#isLeapYear ? 29 : 28;
+    }
+
+    #getCurrentMonthDays(year, currentMonth) {
+        const days = MONTHS[currentMonth];
+
+        const isFebruary = days == 0;
+        if (isFebruary) {
+            return this.#getFebruaryDays(year);
+        }
+
+        return days;
+    }
+
+    #getPreviousMonth(month) {
+        const prevMonth = month - 1;
+
+        if (prevMonth < 0) {
+            return ALL_MONTHS - 1;
+        }
+
+        return prevMonth;
     }
 }
